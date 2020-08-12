508e6a106ee7334633ce79b71e389b0ca419ce29
Bilal-Gujjar <Bilaltahir515@gmail.com>
2020-08-12 12:23:36 +0500

    int variables

---
+++ variable.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+	int x;
+	int y;
+	int z;
+	x = 10;
+	y = 20;
+	z = x+y;
+/*	cout << "x = ";
+	cout << x;
+	cout << "y = ";
+	cout << y;
+	cout << "z = x + y ";*/
+	cout << z;
+}
+
